71092572448c3e529982877e29d7ed0b6da68228
Alexander Alford <xander.alford@gmail.com>
2014-11-25 16:19:38 -0800

    Make nuke-pave delete and recreate space.
    Signed-off-by: Jesse Alford <jalford@pivotallabs.com>

--- main.go
+++ main.go
@@ -2,24 +2,34 @@ package main
 
 import (
 	"fmt"
-
 	"github.com/cloudfoundry/cli/plugin"
+	"strings"
 )
 
-type BasicPlugin struct{}
+type NukePlugin struct{}
 
-func (c *BasicPlugin) Run(cliConnection plugin.CliConnection, args []string) {
+func (c *NukePlugin) Run(cliConnection plugin.CliConnection, args []string) {
 	if args[0] == "nuke-pave" {
-		fmt.Println("circumstances are cyclical\n\ndestroying and rebuilding the space you inhabit")
+		fmt.Println("\ncircumstances are cyclical\n\ndestroying and rebuilding the space you inhabit...")
+		target, err := cliConnection.CliCommandWithoutTerminalOutput("target")
+		if err != nil {
+			fmt.Println("PLUGIN ERROR: Error from CliCommand: ", err)
+		}
+		spaceName := strings.TrimSpace(strings.TrimPrefix(target[4], "Space:"))
+		cliConnection.CliCommandWithoutTerminalOutput("delete-space", "-f", spaceName)
+		cliConnection.CliCommandWithoutTerminalOutput("create-space", spaceName)
+		
+		cliConnection.CliCommand("target", "-s", spaceName)
+		fmt.Println("\nwhat was once is now again")
 	}
 }
 
-func (c *BasicPlugin) GetMetadata() plugin.PluginMetadata {
+func (c *NukePlugin) GetMetadata() plugin.PluginMetadata {
 	return plugin.PluginMetadata{
 		Name: "Nuke-and-Pave",
 		Commands: []plugin.Command{
 			plugin.Command{
-				Name: 	  "nuke-pave",
+				Name:     "nuke-pave",
 				HelpText: "Deletes, recreates, and retargets your space.",
 			},
 		},
@@ -27,5 +37,5 @@ func (c *BasicPlugin) GetMetadata() plugin.PluginMetadata {
 }
 
 func main() {
-	plugin.Start(new(BasicPlugin))
+	plugin.Start(new(NukePlugin))
 }
