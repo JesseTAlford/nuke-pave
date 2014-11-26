355522919c67170ac1467d842e8ac3fdf2fc2c70
Alexander Alford <xander.alford@gmail.com>
2014-11-25 17:02:26 -0800

    Add interactive confirmation.
    Signed-off-by: Jesse Alford <jalford@pivotallabs.com>

--- main.go
+++ main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"github.com/cloudfoundry/cli/plugin"
+	"os"
 	"strings"
 )
 
@@ -10,17 +12,23 @@ type NukePlugin struct{}
 
 func (c *NukePlugin) Run(cliConnection plugin.CliConnection, args []string) {
 	if args[0] == "nuke-pave" {
-		fmt.Println("\ncircumstances are cyclical\n\ndestroying and rebuilding the space you inhabit...")
-		target, err := cliConnection.CliCommandWithoutTerminalOutput("target")
-		if err != nil {
-			fmt.Println("PLUGIN ERROR: Error from CliCommand: ", err)
+		fmt.Println("\ncircumstances are cyclical\n")
+		reader := bufio.NewReader(os.Stdin)
+		fmt.Print("are you certain you wish to evoke creative destruction?\n\n> ")
+		input, _ := reader.ReadString('\n')
+		if input == "y\n" || input ==  "yes\n" || input == "Y\n" || input == "Yes\n" {
+			fmt.Println("\ndestroying and rebuilding the space you inhabit...")
+			target, err := cliConnection.CliCommandWithoutTerminalOutput("target")
+			if err != nil {
+				fmt.Println("PLUGIN ERROR: Error from CliCommand: ", err)
+			}
+			spaceName := strings.TrimSpace(strings.TrimPrefix(target[4], "Space:"))
+			cliConnection.CliCommandWithoutTerminalOutput("delete-space", "-f", spaceName)
+			cliConnection.CliCommandWithoutTerminalOutput("create-space", spaceName)
+			
+			cliConnection.CliCommand("target", "-s", spaceName)
+			fmt.Println("\nwhat was once is now again")
 		}
-		spaceName := strings.TrimSpace(strings.TrimPrefix(target[4], "Space:"))
-		cliConnection.CliCommandWithoutTerminalOutput("delete-space", "-f", spaceName)
-		cliConnection.CliCommandWithoutTerminalOutput("create-space", spaceName)
-		
-		cliConnection.CliCommand("target", "-s", spaceName)
-		fmt.Println("\nwhat was once is now again")
 	}
 }
 
